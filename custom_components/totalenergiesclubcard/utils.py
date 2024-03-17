4835dee8225638c7fc3e7a090e71a5acd07446d0
myTselection <myTselection@gmail.com>
2024-03-17 20:36:42 +0100

    index out of range fix

--- custom_components/totalenergiesclubcard/utils.py
+++ custom_components/totalenergiesclubcard/utils.py
@@ -31,7 +31,7 @@ class ComponentSession(object):
         self.s.headers["Accept-Language"] = "en-US,en;q=0.9"
 
     def login(self, cardnumber, password):
-        response = self.s.get("https://services.totalenergies.be/nl/inloggen-op-uw-club-account",timeout=30,allow_redirects=True)
+        response = self.s.get("https://services.totalenergies.be/nl/inloggen-op-uw-club-account",timeout=30,allow_redirects=True, verify=False)
         
         data = {
             "noCarte": cardnumber,
@@ -42,7 +42,7 @@ class ComponentSession(object):
             "codeCategorie": ""
         }
         _LOGGER.debug(f"post data: {data}")
-        response = self.s.post("https://club.totalenergies.be/authentification/authentification.php?PAYS=BE&LG=NL",data=data,timeout=30,allow_redirects=False)
+        response = self.s.post("https://club.totalenergies.be/authentification/authentification.php?PAYS=BE&LG=NL",data=data,timeout=30,allow_redirects=False, verify=False)
         _LOGGER.debug("post result status code: " + str(response.status_code))
         _LOGGER.debug("post result response: " + str(response.text))
         _LOGGER.debug("post result cookies: " + str(self.s.cookies))
@@ -67,7 +67,7 @@ class ComponentSession(object):
         return details
 
     def transactions(self):
-        response = self.s.get("https://club.totalenergies.be/adherent_transactions/transactions.php?PAYS=BE&LG=NL",timeout=30,allow_redirects=True)
+        response = self.s.get("https://club.totalenergies.be/adherent_transactions/transactions.php?PAYS=BE&LG=NL",timeout=30,allow_redirects=True, verify=False)
         
         _LOGGER.debug("get result status code: " + str(response.status_code))
         _LOGGER.debug("get result response: " + str(response.text))
@@ -81,10 +81,10 @@ class ComponentSession(object):
 
         for row in rows:
             columns = row.find_all('td')
-            if len(columns) == 4 and len(columns[0].contents) >= 2:
+            if len(columns) >= 4:
                 transaction = {
                     'date': columns[0].contents[0],
-                    'station': columns[0].contents[2],
+                    'station': columns[0].contents[2] if len(columns[0].contents) >= 3 else 'Unknown',
                     'subject': columns[1].text.strip(),
                     'debit': int(columns[2].text.strip()),
                     'credit': int(columns[3].text.strip())
